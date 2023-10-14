6a2af41186dcd5c23189d0409d95b1f1e7bb8242
xerayx <119497632+xer4yx@users.noreply.github.com>
2023-10-14 22:33:15 +0800

    Update file_sorter.py

--- file_sorter.py
+++ file_sorter.py
@@ -2,19 +2,27 @@ import os
 import shutil
 import json
 
-
 class Sorter:
     @staticmethod
     def start_sort():
-        with open('path/to/File Sorter/config.json', 'r') as config_file:
-            config = json.load(config_file)
-            source = config["source-dir"]
-            extensionConfig = config.get("file-extension")
-            os.chdir(source)
-            for file in os.listdir():
-                file_classifier = extensionConfig.get(os.path.splitext(file)[1])
-                folder = config.get("dest-folders").get(file_classifier)
-                if file_classifier is not None:
-                    shutil.move(os.path.join(source, file), folder)
-                else:
-                    print(f"{file} cannot be moved due to unspecified type")
+        try:
+            with open('C:/Users/63928/Desktop/File Sorter/config.json', 'r') as config_file:
+                config = json.load(config_file)
+                source = config["source-dir"]
+                extensionConfig = config.get("file-extension")
+                os.chdir(source)
+                for file in os.listdir():
+                    file_classifier = extensionConfig.get(os.path.splitext(file)[1])
+                    folder = config.get("dest-folders").get(file_classifier)
+                    source_path = os.path.join(source, file)
+                    dest_path = os.path.join(folder, file)
+                    if file_classifier is not None:
+                        shutil.move(source_path, dest_path)
+                    else:
+                        print(f"{file} cannot be moved due to unspecified type")
+        except FileNotFoundError:
+            print("Config file not found.")
+        except json.JSONDecodeError:
+            print("Error decoding JSON.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
